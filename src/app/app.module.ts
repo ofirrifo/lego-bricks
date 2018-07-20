6826fc4f2c74e49cb39122ca0f0c9556f65437c1
ofirf <ofirf@sentinel.com>
2018-07-20 10:36:32 +0300

    added tabindex to checkbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { StageModule } from './fetures/stage/stage.module';
 import { LeftPanelModule } from './fetures/left-panel/left-panel.module';
+import { HeaderModule } from './fetures/header/header.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     StageModule,
-    LeftPanelModule
+    LeftPanelModule,
+    HeaderModule
   ],
   declarations: [
     AppComponent
