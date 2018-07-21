0ba46748646d900768c7f8d7b2375bad64240a01
ofirf <ofirf@sentinel.com>
2018-07-21 11:05:25 +0300

    added dropdown trackBy

---
+++ projects/lego-bricks-lib/src/lib/input-module/input/input.component.ts
@@ -0,0 +1,17 @@
+import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'lb-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class InputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
